97ee4651c7a8cbc88193e8055ea48cbbe98ac8b1
dev-swi2021 <77826604+dev-swi2021@users.noreply.github.com>
2021-11-03 17:51:15 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
 from .TTA import TTAWrapper
-from .FPN import FPN
 from .Unet import UnetResNet
 from .trainer import Trainer, Meter
 from .cityscapes_utils import CityscapesLabelEncoder, CityscapesTrainDataset, CityscapesDataset, CityscapesTestDataset
